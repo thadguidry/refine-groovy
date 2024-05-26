788182ea0ab68474e99c7503b96ce9204f8f085c
Thad Guidry <thadguidry@gmail.com>
2024-05-26 08:36:14 +0800

    init GroovyEvaluable with null script

--- src/test/java/io/github/thadguidry/refinegroovy/GroovyEvaluableTests.java
+++ src/test/java/io/github/thadguidry/refinegroovy/GroovyEvaluableTests.java
@@ -14,15 +14,15 @@ import com.google.refine.model.Project;
 import com.google.refine.model.Recon;
 import com.google.refine.model.ReconCandidate;
 import com.google.refine.model.Row;
-
+import io.github.thadguidry.refinegroovy.GroovyEvaluable;
 
 public class GroovyEvaluableTests {
     
-    GroovyEvaluable ge = new GroovyEvaluable();
+    GroovyEvaluable ge = new GroovyEvaluable(null);
     
     protected GroovyEvaluable parse(String source) {
         try {
-            return ge(source);
+            return ge.evaluate(source);
         } catch (ParsingException e) {
             Assert.fail("Parsing the expression raised an unexpected syntax error: "+e.getLocalizedMessage());
             return null;
